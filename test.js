d30bc22fea3b65e7d48d40e71fdd9eaca67612c6
Hasnat Ullah <hasnat.ullah@gmail.com>
2017-06-25 23:25:26 +0100

    use class to keep mainData in instance

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const runSetOfPromisesFrom = require('./index.js')
+const PromisesRunner = require('./index.js')
 const sleepFor = (seconds, value, valueAs, d) => new Promise(resolve => {
     setTimeout(() => {
         console.log('resolving', {[valueAs]: value});
@@ -7,17 +7,20 @@ const sleepFor = (seconds, value, valueAs, d) => new Promise(resolve => {
 });
 
 const allPromises = [
-    {a: (d) => sleepFor(1000, 'action1', 'a1', d), o: 'actionResult1'},
-    {a: (d) => sleepFor(3000, 'action2', 'a2', d), o: 'actionResult2'},
-    {a: (d) => sleepFor(1000, 'action3w', 'a3', d), o: 'actionResult3', w: true},
-    {a: (d) => sleepFor(8000, 'action4', 'a4', d), o: 'actionResult4'},
-    {a: (d) => sleepFor(1000, 'action5', 'a5', d), o: 'actionResult5'},
-    {a: (d) => sleepFor(2000, 'action6w', 'a6', d), o: 'actionResult6', w: true},
-    {a: (d) => sleepFor(2000, 'action7', 'a7', d), o: 'actionResult7'},
-    {a: (d) => sleepFor(5000, 'action8w', 'a8', d), o: 'actionResult8', w: true},
-    {a: (d) => sleepFor(2000, 'action9', 'a9', d), o: 'actionResult9'},
-    {a: (d) => sleepFor(1000, 'action10', 'a10', d), o: 'actionResult10'},
+    {a: (d) => sleepFor(100, 'action1', 'a1', d), o: 'actionResult1'},
+    {a: (d) => sleepFor(300, 'action2', 'a2', d), o: 'actionResult2'},
+    {a: (d) => sleepFor(100, 'action3w', 'a3', d), o: 'actionResult3', w: true},
+    {a: (d) => sleepFor(800, 'action4', 'a4', d), o: 'actionResult4'},
+    {a: (d) => sleepFor(100, 'action5', 'a5', d), o: 'actionResult5'},
+    {a: (d) => sleepFor(200, 'action6w', 'a6', d), o: 'actionResult6', w: true},
+    {a: (d) => sleepFor(200, 'action7', 'a7', d), o: 'actionResult7'},
+    {a: (d) => sleepFor(500, 'action8w', 'a8', d), o: 'actionResult8', w: true},
+    {a: (d) => sleepFor(200, 'action9', 'a9', d), o: 'actionResult9'},
+    {a: (d) => sleepFor(100, 'action10', 'a10', d), o: 'actionResult10'},
 
 ];
-runSetOfPromisesFrom(allPromises, {}, 0);
+let runset = new PromisesRunner(allPromises, {initialData: -1});
 
+runset.runSetOfPromisesFrom()
+    .then(d => console.log('data', d))
+    .catch(e => console.log('e', e));
